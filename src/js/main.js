8bc6417b4eb662670d28491480adb4a0af4ccccc
Veranika Kanaval <veronica22.199@gmail.com>
2025-11-17 00:13:33 +0400

    feat: add html, scss, slider

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import '../scss/style.scss';
+import { initSlider } from './slider.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  initSlider();  
+ 
+});
---
+++ src/js/slider.js
@@ -0,0 +1,23 @@
+import Swiper from 'swiper/bundle';
+
+export function initSlider() {
+    const slider = new Swiper('.swiper-container', {
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        slidesPerView: 1,
+        spaceBetween: 0,
+    });
+
+    return slider;
+}
